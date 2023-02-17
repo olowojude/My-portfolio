eb38009163e71edaf885cf456574f34a2177bf3b
olowojude <olowojude73@gmail.com>
2023-02-17 01:32:40 +0100

    Fix: Fix fonts issues + addition of GSAP animations to 2nd and 3rd section

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-// clip-path: polygon(0 0, 100% 0, 100% 0, 0 0);
+gsap.registerPlugin(ScrollTrigger)
 
-let tl = gsap.timeline({ defaults: { Easings: Expo.Easeout } });
-tl.from(".hero-text", 1, { y: "50%", ease: "power4.easeOut", opacity: 0, delay: 1 }, 0.15)
-    // .from(".nav-text-reveal", 1, { y: "100%", ease: "power4.easeOut", opacity: 0, delay: 1 }, 0.15)
-    .from(".nav-text-reveal", { rotationY: 36, opacity: 0, duration: .8, yPercent: -100, stagger: 0.1, ease: "Expo.easeOut" })
-    .from(".down-links-text-reveal", 1, { y: "50%", ease: "power4.easeOut", opacity: 0, delay: .5 })
-    .from(".second-section-text-container", 1, { opacity: 0 })
+let t = gsap.timeline({
+    defaults: { Easings: Expo.Easeout }
+});
+t.from(".hero-text", 1, { y: "50%", ease: "power4.easeOut", opacity: 0, delay: 1 }, 0.15)
+    .from(".nav-text-reveal", { rotationY: 36, opacity: 0, duration: .5, yPercent: -100, stagger: 0.1, ease: "Expo.easeOut" })
+    .from(".down-links-text-reveal", .5, { y: "50%", ease: "power4.easeOut", opacity: 0, delay: .5 })
+
+gsap.from(".second-section-text-container", {
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".second-section-text-container",
+        scrub: true,
+        start: "top 70%",
+    }
+})
+
+gsap.from(".my-pic", {
+    x: -100,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".my-pic",
+        toggleActions: "play reset play reset",
+        markers: true,
+        start: "top 90%",
+    }
+})
+
+gsap.from(".title-heading", {
+    opacity: 0,
+    duration: .5,
+    scrollTrigger: {
+        trigger: ".title-heading",
+        scrub: true,
+        start: "top 90%",
+    }
+})
+
+gsap.from(".third-section-paragragh", {
+    opacity: 0,
+    duration: .2,
+    scrollTrigger: {
+        trigger: ".third-section-paragragh",
+        scrub: true,
+    }
+})
